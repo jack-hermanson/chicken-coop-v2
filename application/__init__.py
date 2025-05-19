c07235befa88d446be58600615aa78ad0936993c
Jack Hermanson <jack.hermanson@live.com>
2025-05-18 20:51:43 -0600

    more stuff

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,10 @@
-from flask import Flask
+import os
+import subprocess
+
+from flask import Flask, request
 
 from Config import Config
+from utils.get_ip import get_ip
 
 
 def create_app(config_class=Config):
@@ -8,10 +12,25 @@ def create_app(config_class=Config):
 
     app.config.from_object(config_class)
 
+    @app.context_processor
+    def inject_environment():
+        return dict(
+            commit=subprocess.check_output(["git", "describe", "--always"]).strip().decode("utf-8"),
+            environment=os.environ.get("ENVIRONMENT"),
+            # get_ip=get_ip
+        )
+
+    # todo - remove this
     @app.route("/")
     def index():
-        return "hi"
-        return "<body style='background-color: #222; color: #eee;>hello</body>'"
+        # return "hi"
+        return f"""
+        <html style="background-color: #222; color: #ddd; font-family: sans-serif;">
+        <body>
+        <h1>Hi {get_ip(request=request)}</h1>
+        </body>
+        </html>
+        """
 
     print("RUNNING APPLICATION")
     return app
