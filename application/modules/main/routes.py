8070a92db7d0932ab19d94d6302fcd3ccac383f3
Jack Hermanson <jack.hermanson@live.com>
2025-06-15 20:27:57 -0600

    add pwa stuff

--- application/modules/main/routes.py
+++ application/modules/main/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, redirect, render_template, url_for
+from pathlib import Path
+
+from flask import Blueprint, redirect, render_template, send_file, url_for
 from flask.typing import ResponseReturnValue
 from flask_login import current_user
 
@@ -23,3 +25,15 @@ def info() -> ResponseReturnValue:
 @main.route("/error-500")
 def error_500() -> ResponseReturnValue:
     raise ValueError("Error on purpose")
+
+
+@main.route("/manifest.json")
+def serve_manifest() -> ResponseReturnValue:
+    path = Path("./application/web/static/manifest.json")
+    return send_file(path.resolve(strict=True), mimetype="application/manifest+json")
+
+
+@main.route("/sw.js")
+def serve_sw() -> ResponseReturnValue:
+    path = Path("./application/web/static/sw.js")
+    return send_file(path.resolve(strict=True), mimetype="application/javascript")
