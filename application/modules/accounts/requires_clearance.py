3c698cd39ebed2de0e36bc9c2adaac92b4c3cac7
Jack Hermanson <jack.hermanson@live.com>
2025-06-15 20:10:27 -0600

    add ability to log in with email

--- application/modules/accounts/requires_clearance.py
+++ application/modules/accounts/requires_clearance.py
@@ -2,7 +2,7 @@ from collections.abc import Callable
 from functools import wraps
 from http import HTTPStatus
 
-from flask import abort, request
+from flask import abort, redirect, request, url_for
 from flask_login import current_user
 
 from application import logger
@@ -15,7 +15,7 @@ def requires_clearance(minimum_clearance: ClearanceEnum) -> Callable:
         def wrapped(*args, **kwargs) -> Callable:
             if not current_user.is_authenticated:
                 logger.warning(f"Anonymous user tried to access {request.path}")
-                return abort(HTTPStatus.UNAUTHORIZED)
+                return redirect(url_for("accounts.login", next=request.path))
             if not current_user.clearance >= minimum_clearance:
                 logger.warning(f"<{current_user.username}, {current_user.account_id}> tried to access {request.path}")
                 return abort(HTTPStatus.FORBIDDEN)
