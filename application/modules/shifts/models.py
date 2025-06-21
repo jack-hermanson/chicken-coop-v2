11a9ace1b43a5816886258116caf37210fb6cd99
Jack Hermanson <jack.hermanson@live.com>
2025-06-20 22:55:07 -0600

    wip

--- application/modules/shifts/models.py
+++ application/modules/shifts/models.py
@@ -1,8 +1,11 @@
-from datetime import date, datetime
+from __future__ import annotations
+
+from datetime import date, datetime  # noqa: TC003
 from enum import IntEnum
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Date, DateTime, ForeignKey, Integer, String, text
+from sqlalchemy import Case, Date, DateTime, ForeignKey, Integer, String, case, text
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from application import db
@@ -25,18 +28,35 @@ class Shift(db.Model):
     comments: Mapped[str] = mapped_column(String(255), nullable=False, server_default="", default="")
     eggs_collected: Mapped[int] = mapped_column(Integer, nullable=True)
     eggs_left_behind: Mapped[int] = mapped_column(Integer, nullable=True)
-    # completed_datetime_utc: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
+    completed_datetime_utc: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
 
     # Relationship back to Account for list of assigned shifts
     assigned_to_account_id: Mapped[int] = mapped_column(ForeignKey("account.account_id"), nullable=True)
-    assigned_to_account: Mapped["Account"] = relationship(back_populates="assigned_shifts", lazy="select")
+    assigned_to_account: Mapped[Account] = relationship(back_populates="assigned_shifts", lazy="select")
+
+    # Who completed this shift
+    completed_by_account_id: Mapped[int] = mapped_column(ForeignKey("account.account_id"), nullable=True)
+    completed_by_account: Mapped[Account] = relationship(lazy="select")
 
     # If this shift has a coverage request
-    coverage_request: Mapped["CoverageRequest"] = relationship(back_populates="shift", uselist=False)
+    coverage_request: Mapped[CoverageRequest] = relationship(back_populates="shift", uselist=False)
 
     # Relationship back to ShiftAssignment for list of specific shifts
     shift_assignment_id: Mapped[int] = mapped_column(ForeignKey("shift_assignment.shift_assignment_id"), nullable=True)
-    shift_assignment: Mapped["ShiftAssignment"] = relationship(back_populates="shifts", lazy="select")
+    shift_assignment: Mapped[ShiftAssignment] = relationship(back_populates="shifts", lazy="select")
+
+    # Computed column to tell you if this shift was completed
+    @hybrid_property
+    def completed(self) -> bool:
+        return self.completed_datetime_utc is not None
+
+    # noinspection PyMethodParameters
+    @completed.expression
+    def completed(cls: type[Shift]) -> Case:  # noqa: N805
+        return case(
+            (cls.completed_datetime_utc.is_not(None), True),
+            else_=False,
+        )
 
 
 class CoverageRequest(db.Model):
@@ -63,11 +83,11 @@ class CoverageRequest(db.Model):
 
     # Relationship back to Shift
     shift_id: Mapped[int] = mapped_column(ForeignKey("shift.shift_id"), nullable=False, unique=True)
-    shift: Mapped["Shift"] = relationship(back_populates="coverage_request", lazy="select")
+    shift: Mapped[Shift] = relationship(back_populates="coverage_request", lazy="select")
 
     # Covered by
     covered_by_account_id: Mapped[int] = mapped_column(ForeignKey("account.account_id"), nullable=True)
-    covered_by_account: Mapped["Account"] = relationship(back_populates="accepted_coverage_requests", lazy="select")
+    covered_by_account: Mapped[Account] = relationship(back_populates="accepted_coverage_requests", lazy="select")
 
 
 class TimeOfDayEnum(IntEnum):
@@ -99,13 +119,15 @@ class ShiftAssignment(db.Model):
     shift_assignment_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     time_of_day: Mapped[TimeOfDayEnum] = mapped_column(Integer, nullable=False)
     day_of_week: Mapped[DayOfWeekEnum] = mapped_column(Integer, nullable=False)
+    comments: Mapped[str] = mapped_column(String(255), nullable=False, server_default="", default="")
+    seeking_permanent_replacement: Mapped[bool] = mapped_column()
 
     # Relationship back to Account for list of assigned shifts
     account_id: Mapped[int] = mapped_column(ForeignKey("account.account_id"), nullable=True)
-    account: Mapped["Account"] = relationship(back_populates="assignments", lazy="select")
+    account: Mapped[Account] = relationship(back_populates="assignments", lazy="select")
 
     # Children
-    shifts: Mapped[list["Shift"]] = relationship(
+    shifts: Mapped[list[Shift]] = relationship(
         back_populates="shift_assignment",
         lazy="dynamic",
         cascade="all, delete-orphan",
