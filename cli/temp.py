71d14023419c29beba7797c12e867d51dc2a9c8d
Jack Hermanson <jack.hermanson@live.com>
2025-06-29 00:46:25 -0600

    remove temp

--- cli/temp.py
+++
@@ -1,7 +0,0 @@
-from application import create_app
-from application.modules.shifts.services.generate_shifts import generate_shifts
-
-if __name__ == "__main__":
-    app = create_app()
-    with app.app_context():
-        generate_shifts()
